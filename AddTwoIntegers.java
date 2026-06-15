506a017bccfaf39fab94a44cabe693650397251a
Shubham Balasaheb Bhavar <shubhambhavar108@gmail.com>
2026-06-15 13:28:33 +0530

    Create AddTwoIntegers.java to sum two integers

---
+++ AddTwoIntegers.java
@@ -0,0 +1,9 @@
+//Accept Two integers and add it
+
+class Solution {
+    public int sum(int num1, int num2) //Accept two numbers
+    {
+        int sum=num1+num2;//add it
+        return sum;//return sum
+    }
+}
