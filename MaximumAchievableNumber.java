d7107c7415eacb48c98fca29b07dcfc19fe4df8e
Shubham Balasaheb Bhavar <shubhambhavar108@gmail.com>
2026-06-23 14:46:10 +0530

    Add problem description and approach comments
    Added problem statement and approach for Maximum Achievable Number.

--- MaximumAchievableNumber.java
+++ MaximumAchievableNumber.java
@@ -1,4 +1,15 @@
 
+/*Problem: The Maximum Achievable Number
+
+Description:
+Given two integers num and t, return the maximum possible value of x
+that can become equal to num after applying the given operation
+at most t times.
+
+Approach:
+Each operation increases the maximum achievable value by 2.
+Therefore, the answer is:
+num + (2 * t)*/
 
 class Solution {
     public int theMaximumAchievableX(int num, int t) {
