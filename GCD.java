83b6c9d107d33dbf723df45e1de877b1dddcb7ce
Shubham Balasaheb Bhavar <shubhambhavar108@gmail.com>
2026-07-17 07:59:06 +0530

    Create GCD.java

---
+++ GCD.java
@@ -0,0 +1,21 @@
+/**
+ * 🧠 Problem:
+ * Return GCD of:
+ * - sum of first n odd numbers
+ * - sum of first n even numbers
+ *
+ * ⚡ Math Insight:
+ * sumOdd  = n^2
+ * sumEven = n * (n + 1)
+ *
+ * GCD(n^2, n(n+1)) = n
+ *
+ * ⏱ Time: O(1)
+ * ⏱ Space: O(1)
+ */
+
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+}
