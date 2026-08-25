420a542f7bea5182c3769c4bf671093a8c93be60
Shubham Balasaheb Bhavar <shubhambhavar108@gmail.com>
2026-08-25 10:11:22 +0530

    Create Find_The_Peaks.java

---
+++ Easy/Find_The_Peaks.java
@@ -0,0 +1,28 @@
+    /*
+     * Question:
+     * Find all peak indices in the mountain array.
+     *
+     * A peak is strictly greater than both neighbors.
+     * The first and last elements cannot be peaks.
+     */
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+
+    public List<Integer> findPeaks(int[] mountain) {
+        List<Integer> peaks = new ArrayList<>();
+
+        // Check only elements between first and last
+        for (int i = 1; i < mountain.length - 1; i++) {
+
+            if (mountain[i] > mountain[i - 1]
+                    && mountain[i] > mountain[i + 1]) {
+
+                peaks.add(i);
+            }
+        }
+
+        return peaks;
+    }
+}
